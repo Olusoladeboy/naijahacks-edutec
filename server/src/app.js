5d1cccc094c817dd6418d152b9ee215892e44a56
Olusoladeboy <eoyewole508@stu.ui.edu.ng>
2019-12-11 10:32:36 +0100

    Created POST route for signing up user

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,26 @@
-console.log('app works');
+const express = require('express');
+require('../db/mongoose')
+const hbs = require('hbs')
+const app = express();
+const User = require('../models/user')
+
+
+const port = process.env.PORT || 8081;
+
+app.get("/", (req, res) => {
+    res.status(200).send("This is the landing page and database is connected")
+})
+
+app.post("/user", async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send({ user })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.listen(port, () => {
+    console.log("App is running on localhost:" + port)
+})
